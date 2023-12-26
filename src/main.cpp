9a99584bdbfc6e7a61839397f28cb6978319b4bd
hu263 <hu263@uregina.ca>
2023-12-26 20:21:42 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+class inner
+{
+public:
+  inner() { cout << "Constructing" << endl; }
+  ~inner() { cout << "Destructing" << endl; }
+};
+
+int main(int argc, char **argv) {
+  inner *p = new inner();
+  delete p;
+
+
+  inner *pa = new inner[2];
+  delete [] pa;
+
+  return 0;
+}
